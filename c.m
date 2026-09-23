cfbd7c07fde5f071b476b920d228cfb689d931e8
kyadasipallavi-ai <kyadasipallavi@gmail.com>
2026-09-23 14:15:57 +0530

    Create c.m

---
+++ c.m
@@ -0,0 +1,12 @@
+<h3>SHA-256 Hash Demo</h3>
+
+<p>
+    Enter a sample password below to see how hashing converts it into a
+    fixed-length value.
+</p>
+
+<input type="text" id="hashInput" placeholder="Enter sample password">
+
+<button id="hashBtn">Generate Hash</button>
+
+<p id="hashOutput"></p>
